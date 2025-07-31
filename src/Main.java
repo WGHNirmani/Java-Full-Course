50c9606ee1a710f86c603c10c73d0ae01eb36ccc
WGHNirmani <hirunikanirmani26@gmail.com>
2025-07-31 11:22:30 +0530

    #1 arithmetic

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,27 @@ public class Main {
         multi line
         comment
          */
-        System.out.println("I like pizza!");
-        System.out.println("Its Really Good!");
-        System.out.println("Buy me pizza!");
+
+//        System.out.println("I like pizza!");
+//        System.out.println("Its Really Good!");
+//        System.out.println("Buy me pizza!");
+
+        // variable = a reusable container for a value
+        //            a variable behaves as if it was the value it contains
+
+        // Primitive = simple value stored directly in memory (stack)
+        // Reference = memory address (stack) that points to the (heap)
+
+        // Primitive vs Reference
+        // ---------    ---------
+        // int          String
+        // double       array
+        // char         object
+        // boolean
+
+        // 2 steps to creating a variable
+        //
+        // 1.declaration
+        // 2. assignment
     }
 }
