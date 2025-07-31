f1f805c393ff34f82b3ca900e09dc11a180f0517
WGHNirmani <hirunikanirmani26@gmail.com>
2025-07-31 11:21:34 +0530

    arithmetic

---
+++ src/Arithmetic.java
@@ -0,0 +1,56 @@
+public class Arithmetic {
+    public static void main(String [] args){
+        // Arithmetic Operators
+
+//        int x = 10;
+//        int y = 2;
+//        int z;
+
+        // Z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x /y;
+        //z = x % y;
+        //System.out.println(z);
+
+        // Augmented Assignment Operators
+
+//        int x = 10;
+//        int y = 3;
+
+//        double x = 10;
+//        double y = 3;
+
+        // x = x + y;
+        // x += y;
+        // x = x - y;
+        // x -= y
+        // x = x * y;
+        // x += y;
+        // x = x /y;
+        // x /= y;
+        // x = x % y;
+        //x %= y;
+
+        // Increment and Decrement Operators
+
+        int x = 1;
+
+        //x = x + 1;
+        //x += 1;
+        //x++;
+
+        // x = x - 1;
+        // x -= 1;
+        //x --;
+
+        //System.out.println(x);
+
+        // Order of Operations [P-E-M-D-A-S]
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        System.out.println(result);
+
+
+    }
+}
