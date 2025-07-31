35e5aa10e274617f6225ee6dbab92719f9f9b2fe
WGHNirmani <hirunikanirmani26@gmail.com>
2025-07-31 06:12:44 +0530

    introduction to java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String [] args){
+        // This is My First Java Program
+
+        /*
+        This
+        is
+        a
+        multi line
+        comment
+         */
+        System.out.println("I like pizza!");
+        System.out.println("Its Really Good!");
+        System.out.println("Buy me pizza!");
+    }
+}
